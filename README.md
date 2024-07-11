a704c04f770783f8cf8686f0c08b3002b5000249
ctcl-bregis <brayden.regis@gmail.com>
2024-07-11 15:50:57 -0400

    new build script

--- README.md
+++ README.md
@@ -2,62 +2,21 @@
 These are pixel art fonts I have created for a variety of purposes.
 
 Files:
-- .c files: These are the C array output from Piskel that is converted by the [piskelc2svg.py script](https://github.com/ctcl-bregis/script-dump/blob/main/converters/piskelc2svg.py)
-- .svg files: These are the SVG representaion of each glyph
-- .sfd files: These files are the FontForge save used for putting together the font
+- .bdf files: Output of FontForge to be converted to TTF by bdf2ttf.py or used in embedded devices
+- .ttf files: Output of bdf2ttf.py for general use
 
-## Icons-16x16
-Icons-16x16 is a collection of 16x16 pixel art icons
+## Terminology
 
-## Icons-12x12
-Icons-12x12 is a collection of 12x12 pixel art icons
-
-## Pixel-7x7
-Pixel-7x7, inspired by the UI font used in the game *Minecraft* and some fonts used for Flipper Zero tools. It is used on my website for very small text while formats for embedded devices such as [U8g2](https://github.com/olikraus/u8g2/) are planned.
-
-### Monospace
-The Monospace version of Pixel-7x7 is the same as Condensed though with a few characters modified and all of the glyphs were set to the same width.
-
-- 5x9 grid including descent
-
-### True Monospace
-This version is similar to the Monospace font but is always within the 5x7 grid.
-
-## Pixel-9x9
-Pixel-9x9 was made in an attempt to make a slightly larger font for use on the website.
-
-### Supercondensed
-This font is basically the condensed version of Pixel-7x7 but nine pixels tall (excluding descent).
-
-- 5x9 grid, 5x13 grid with descent
-
-## Pixel-12x12
-This font was somewhat inspired by VCR OSD Mono that is recently known for its use in the game *ULTRAKILL*. It was also somewhat inspired by fonts used by Flipper Devices.
-
-### Condensed
-This font finds the most use on my website.
-
-- 8x16 grid including descent
-
-## Pixel-14x14
-This font was created to have a font that is slightly larger than the Pixel-12x12 font. It takes more inspiration from the VCR OSD Mono font already mentioned.
-
-### Condensed
-
-- 10x18 grid including descent
-
-### True Monospace
-Also known as "ULTRACOMPUTER". This font sticks to the 10x14 grid and is more like the VCR OSD Mono font.
-
-- 10x14 grid
-
-## Pixel-17x17
-This font is once again made for having larger text.
-
-### Condensed
-- 13x23 grid including descent
+- Condensed - Each glyph is up to 70%-80% of the grid width
+- Monospace - These fonts are "Condensed" sized but every glyph is the same width
+- Ultra Condensed - Each glyph is up to 50%-60% of the grid width
+- True - These fonts are within the specified size by having no descent value. These fonts are better suited for embedded devices.
 
 
+## Icons
+Starting July 8, 2024, icons are now embedded in fonts with the specified resolution., making use of the Private Use Area blocks of Unicode.
 
+Icons that represent existing emoji are to use code points of such emoji. Custom icons are to be stored at U+E700 to U+E7FF.
 
+What fonts use icons and what codepoints they are assigned are defined in config.json
 
---
+++ build.py
@@ -0,0 +1,156 @@
+# Pixel Fonts - CTCL 2022-2024
+# Purpose: Build script for all fonts and icons
+# Created: July 7, 2024
+# Modified: July 11, 2024
+
+import io
+import json
+import os
+import shutil
+
+import bdfparser
+import drawsvg
+import fontforge
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def replace_right(text, old, new, count=-1):
+    return text[::-1].replace(old[::-1], new[::-1], count)[::-1]
+
+def piskelc2svg(filepath, newfilepath):
+    with open(filepath) as f:
+        content = f.readlines()
+
+    width = int(content[3].split(" ")[2])
+    height = int(content[4].split(" ")[2])
+
+    data = "".join(content[10:]).replace("\n", "").split("}")[0].split(", ")
+    data = [pix[2:] for pix in data]
+    # Data is exported for little-endian CPU architectures and must be "reversed"
+    tmp = []
+    for pix in data:
+        ba = bytearray.fromhex(pix)
+        ba.reverse()
+        tmp.append(ba.hex())
+    data = tmp
+
+    rows = chunks(data, width)
+
+    d = drawsvg.Drawing(width * 64, height * 64)
+
+    ypos = 0
+    xpos = 0
+    for y in rows:
+        for x in y:
+            color = f"#{x[:-2]}"
+            opacity = int(x[-2:], 16) / 256
+            if opacity > 0:
+                d.append(drawsvg.Rectangle(xpos, ypos, 64, 64, fill = color, fill_opacity = opacity))
+            xpos += 64
+        xpos = 0
+        ypos += 64
+
+    d.save_svg(newfilepath)
+
+    os.system(f"inkscape --export-type=svg --export-plain-svg --export-overwrite -g --batch-process {newfilepath} --verb='EditSelectAll;SelectionUnion;FileSave'")
+
+def bdf2vecs(path, output, exts):
+    bdffont = bdfparser.Font(path)
+    glyphs = bdffont.glyphs
+
+    ttffont = fontforge.font()
+    ttffont.encoding = "UnicodeFull"
+    ttffont.fontname = bdffont.props["font_name"]
+    ttffont.familyname = bdffont.props["face_name"]
+    ttffont.fullname = bdffont.headers["fontname"]
+    ttffont.ascent = 64 * int(bdffont.props["font_ascent"])
+    ttffont.descent = 64 * int(bdffont.props["font_descent"])
+
+    svgglyphs = {}
+    for x, y in glyphs.items():
+        glyph = bdffont.glyphbycp(y[1])
+
+        glyph_width = glyph.meta["dwx0"]
+        glyph_height = int(bdffont.props["pixel_size"])
+
+        if glyph_width > 1:
+            rows = []
+            for i in range((glyph_height - glyph.meta["bbh"]) - int(bdffont.props["font_descent"]) - glyph.meta["bbyoff"]):
+                rows.append("0" * glyph.meta["dwx0"])       
+
+            for datarow in glyph.meta["hexdata"]:
+                if datarow == "00":
+                    rows.append("0" * glyph.meta["dwx0"])
+                else:
+                    rows.append(("0" * glyph.meta["bbxoff"]) + str(bin(int(datarow, 16))[2:]).rjust(len(datarow) * 4, "0"))
+
+            d = drawsvg.Drawing(glyph.meta["dwx0"] * 64, glyph_height * 64)
+            ypos = 0
+            xpos = 0
+            notempty = False
+            for row in rows:
+                for pix in row:
+                    if pix == "1":
+                        notempty = True
+                        d.append(drawsvg.Rectangle(xpos, ypos, 64, 64, fill = "#000000", fill_opacity = 255))
+                    xpos += 64
+                xpos = 0
+                ypos += 64
+        
+
+            d.save_svg("/dev/shm/" + y[0] + ".svg")
+
+            g = ttffont.createMappedChar(y[1])
+            if notempty:
+                g.importOutlines("/dev/shm/" + str(y[0]) + ".svg", ('removeoverlap', 'correctdir'))
+            g.width = 64 * (int(glyph.meta["dwx0"]))
+            g.removeOverlap()
+
+            for ext in exts:
+                ttffont.generate(f"{output}.{ext}")
+
+with open("config.json") as f:
+    cfg = dict(json.loads(f.read()))
+
+if os.path.exists("build/"):
+    shutil.rmtree("build/")
+
+if not os.path.exists("build/"):
+    os.mkdir("build/")
+
+for iconset in cfg["icons"]["list"]:
+    filelist = [f for f in os.listdir(iconset) if os.path.isfile(os.path.join(iconset, f))]
+    filelist = [f for f in filelist if f.endswith(".c")]
+
+    if not os.path.exists(f"build/{iconset}"):
+        os.mkdir(f"build/{iconset}/")
+
+    for icon in filelist:
+        piskelc2svg(f"{iconset}/{icon}", f"build/{iconset}/" + icon[:-2] + ".svg")
+
+    
+for (name, data) in cfg["fonts"].items():
+    os.mkdir("build/" + name)
+
+    for (variant, path) in data["variants"].items(): 
+        sfd = fontforge.open(f"{name}/{path}")
+
+        if len(sfd.bitmapSizes) == 1:
+            bmpres = sfd.bitmapSizes[0]
+        elif len(sfd.bitmapSizes) > 1: 
+            print(f"Error: font variant {variant} has more than one bitmap strike resolution, skipping")
+            continue
+        elif len(sfd.bitmapSizes) < 1:
+            print(f"Error: font variant {variant} does not have any bitmap strikes, skipping")
+            continue
+
+        sfd.generate(f"build/{name}/{name}-{variant}.bdf", str(bmpres)) 
+        os.rename(f"build/{name}/{name}-{variant}-{bmpres}.bdf", f"build/{name}/{name}-{variant}.bdf")
+        bdf2vecs(f"build/{name}/{name}-{variant}.bdf", f"build/{name}/{name}-{variant}", cfg["vecs"])
+
+
+            
+
+
